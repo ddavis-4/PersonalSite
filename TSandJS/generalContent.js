2e58845373f937404a8b527ba40665745d030377
ddavis-4 <123651199+ddavis-4@users.noreply.github.com>
2025-01-26 20:54:33 -0600

    WHAT is he building in there

--- TSandJS/generalContent.js
+++ TSandJS/generalContent.js
@@ -9,8 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
         const generalContent = `
             <h4>Welcome to My Pro-tfolio!</h4>
             <p>Sorry! We're under construction.</p>
-            <p>If you click this link from the 'Home' or index page it won't work.</p>
-            <p>Soon to fix!</p>
             <a href="../pages/Resume.pdf">Resume</a>
         `;
         contentDiv.innerHTML += generalContent; // Append dynamic content to the existing content
