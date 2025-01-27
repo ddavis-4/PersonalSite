54a49813a5711f0358667d5f1fbe22d196b16247
ddavis-4 <123651199+ddavis-4@users.noreply.github.com>
2025-01-26 19:05:02 -0600

    Fixes

---
+++ js/mainContent.js
@@ -0,0 +1,16 @@
+// This is the file for the main message on the index page.
+//
+//
+//
+document.addEventListener("DOMContentLoaded", function() {
+    const contentDiv = document.querySelector('.content'); // needs to be on the page to display the message
+
+    // Example of generating content dynamically
+    const mainContent = `
+        <h4>Welcome to My Pro-tfolio!</h4>
+        <p>Sorry! We're under construction.</p>
+        <a href="pages/Resume.pdf">Resume</a>
+    `;
+
+    contentDiv.innerHTML += mainContent; // Append main content to the existing content
+});
